01b126b8af55ff619c6493ef2493a1774a4bc0ad
Fomenko Alex <fama0099@gmail.com>
2022-06-17 21:31:35 +0300

    Добавил комментарии к коду

--- test_fixture1.py
+++ test_fixture1.py
@@ -3,14 +3,16 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-class TestMainPage1():
+class TestMainPage1():  # первый тестСьюит
 
-    @classmethod
+    @classmethod  # создаем функцию инициализации браузера, оборачиваем в декоратор, будет применяться один раз на весь
+                  # класс
     def setup_class(self):
         print("\nstart browser for test suite..")
         self.browser = webdriver.Chrome()
 
-    @classmethod
+    @classmethod  # создаем функцию закрытия браузера, оборачиваем в декоратор, будет применяться один раз на весь
+                  # класс
     def teardown_class(self):
         print("quit browser for test suite..")
         self.browser.quit()
@@ -24,13 +26,15 @@ class TestMainPage1():
         self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
 
 
-class TestMainPage2():
+class TestMainPage2():  # второй тестСьюит
 
-    def setup_method(self):
+    def setup_method(self):  # создаем функцию инициализации браузера, не оборачиваем в декоратор, будет применяться
+                             # к каждому тесту по отдельности в классе
         print("start browser for test..")
         self.browser = webdriver.Chrome()
 
-    def teardown_method(self):
+    def teardown_method(self):# создаем функцию закрытия браузера, не оборачиваем в декоратор, будет применяться
+                              # к каждому тесту по отдельности в классе
         print("quit browser for test..")
         self.browser.quit()
 
@@ -41,3 +45,5 @@ class TestMainPage2():
     def test_guest_should_see_basket_link_on_the_main_page(self):
         self.browser.get(link)
         self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+# запускаем в терминале командой pytest -s test_fixture1.py параметр -s позволяет увидеть вывод print()
