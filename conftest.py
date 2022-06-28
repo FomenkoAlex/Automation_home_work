38f104dbec4bffc2a70e5e1cbc052d361c2e1171
Fomenko Alex <fama0099@gmail.com>
2022-06-28 14:46:56 +0300

    Передача параметров в командной строке.

--- conftest.py
+++ conftest.py
@@ -7,4 +7,31 @@ def browser():
     yield browser
     browser.quit()
 
+#  функция которарая обрабатыет опции
+# def pytest_addoption(parser):
+#     parser.addoption('--browser_name', action='store', default=None,
+#                      help="Choose browser: chrome or firefox")
+
+#  фикстура позволяющая пользоваться разным браузером для тестов, фикстура передает в опции данные, которые
+#  обрабатывает функция выше
+# @pytest.fixture(scope="function")
+# def browser(request):
+#     browser_name = request.config.getoption("browser_name")
+#     browser = None
+#     if browser_name == "chrome":
+#         browser = webdriver.Chrome()
+#     elif browser_name == "firefox":
+#         browser = webdriver.Firefox()
+#     else:
+#         raise pytest.UsageError("--browser_name should be chrome or firefox")
+#     yield browser
+#     browser.quit()
+
+#  при запуске файла с тестами, нужно обязательно указать параметр --browser_name=название браузера
+#  иначе будет ошибка _pytest.config.UsageError: --browser_name should be chrome or firefox
+
+#  Если в функции обработчике опций в default выставить одно из названий браузера, то тесты будут запускаться
+#  на нем автоматически и можно будет запускать файл с тестами без параметра, но при необходимости
+#  выбора другого браузера нужно будет указать его в параметрах при запуске
+
 #  в этом файле мы записываем часто вызываемые фикстуры, для того что б не прописывать их в каждом файле с тестами
